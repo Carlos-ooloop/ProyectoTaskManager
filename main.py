204427447b78386a4189a099bd7d08793c84e775
Carlosa <carlosg050316@gmail.com>
2026-06-12 23:37:08 -0400

    Modificando el main para incluir los Routers

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from db.database import engine
 from models.user_model import User
+from fastapi import FastAPI
+from Routers import Task_CRUD, users_CRUD
+from utils import auth
 
 User.metadata.create_all(bind = engine)
+
+app = FastAPI()
+
+app.include_router(Task_CRUD.router)
+app.include_router(users_CRUD.router)
+app.include_router(auth.router)
+
+
+
+
